122b0f241906a36017d036a117b1d1e59cd9bcf1
Waleed <waleedamjad99@hotmail.com>
2022-05-09 19:34:12 +0500

    Close Container on clicking links

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,7 @@
 const button = document.querySelector('.toolbar__menu-button');
 const closingButton = document.querySelector(".toolbar__menu-close-button")
 const linksList = document.querySelector('.toolbar__links-list');
+const links = document.querySelectorAll(".toolbar__links-list-item");
 
 button.addEventListener('click', open);
 button.addEventListener("click", close);
@@ -12,3 +13,8 @@ function open () {
 function close () {
   linksList.style.display = 'none';
 }
+
+links.forEach (link => {
+  link.addEventListener("click", close)
+});
+
