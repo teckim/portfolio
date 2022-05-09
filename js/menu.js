b30aef9da0101dc0ab6b383588dcedcb22e2d9b2
BHDHakim <hakim.bhd@gmail.com>
2022-05-09 18:46:19 +0100

    Fix Eslint errors

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,16 @@
 const menubutton = document.querySelector('.toolbar__menu-button');
-const closeButton = document.querySelector(".toolbar__close-button")
+const closeButton = document.querySelector('.toolbar__close-button');
 const listContainer = document.querySelector('.toolbar__links-list-container');
-const links = document.querySelectorAll(".toolbar__links-list-item");
+const links = document.querySelectorAll('.toolbar__links-list-item');
 
-menubutton.addEventListener('click', open);
-closeButton.addEventListener("click", close);
-links.forEach (link => link.addEventListener("click", close));
-
-function open () {
+function open() {
   listContainer.style.display = 'flex';
 }
 
-function close () {
+function close() {
   listContainer.style.display = 'none';
 }
+
+menubutton.addEventListener('click', open);
+closeButton.addEventListener('click', close);
+links.forEach((link) => link.addEventListener('click', close));
