b3a326fc6f99c4e8d5fc0eb8519e33415351d037
mr-abdellah <belkaid.abdulah@gmail.com>
2022-05-09 15:22:36 +0100

    Adding closing button feature

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,14 @@
 const button = document.querySelector('.toolbar__menu-button');
+const closingButton = document.querySelector(".toolbar__menu-close-button")
 const linksList = document.querySelector('.toolbar__links-list');
 
 button.addEventListener('click', open);
+button.addEventListener("click", close);
 
 function open () {
-  linksList.style.display = 'flex'
+  linksList.style.display = 'flex';
+}
+
+function close () {
+  linksList.style.display = 'none';
 }
