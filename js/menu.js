b37612583750c083e82b262e46d2613520eaf666
BHDHakim <hakim.bhd@gmail.com>
2022-05-09 17:59:13 +0100

    Refactor

--- js/menu.js
+++ js/menu.js
@@ -1,20 +1,16 @@
-const button = document.querySelector('.toolbar__menu-button');
-const closingButton = document.querySelector(".toolbar__menu-close-button")
-const linksList = document.querySelector('.toolbar__links-list');
+const menubutton = document.querySelector('.toolbar__menu-button');
+const closeButton = document.querySelector(".toolbar__close-button")
+const listContainer = document.querySelector('.toolbar__links-list-container');
 const links = document.querySelectorAll(".toolbar__links-list-item");
 
-button.addEventListener('click', open);
-button.addEventListener("click", close);
+menubutton.addEventListener('click', open);
+closeButton.addEventListener("click", close);
+links.forEach (link => link.addEventListener("click", close));
 
 function open () {
-  linksList.style.display = 'flex';
+  listContainer.style.display = 'flex';
 }
 
 function close () {
-  linksList.style.display = 'none';
+  listContainer.style.display = 'none';
 }
-
-links.forEach (link => {
-  link.addEventListener("click", close)
-});
-
