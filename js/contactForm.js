7f61c0717748f8273255d101858bb8da9a31f41e
mr-abdellah <belkaid.abdulah@gmail.com>
2022-05-11 14:38:02 +0100

    Adding email validator function

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,3 +1,17 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
 const error = document.getElementById('errors');
+
+form.addEventListener('submit', (event) => {
+    let a = false;
+    event.preventDefault();
+    console.log(email.value,email.value.toLowerCase());
+    if(email.value === email.value.toLowerCase()) {
+       a = true;
+    }
+    if(a===false)
+    {
+    error.innerText = 'Please Your Email Must be in Lower Case';
+    }
+  } );
+  
