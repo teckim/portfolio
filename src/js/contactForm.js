cbc4d8c253542792fa2eb9269f9dbd612f9fc82b
BHDHakim <hakim.bhd@gmail.com>
2022-05-11 18:27:03 +0100

    Apply refinements

--- src/js/contactForm.js
+++ src/js/contactForm.js
@@ -1,12 +1,16 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
-const error = document.getElementById('errors');
+const errors = document.getElementById('errors');
 
 form.addEventListener('submit', (event) => {
   const isFormValid = email.value === email.value.toLowerCase();
 
+  errors.classList.remove('contact__form-errors--visible');
+
   if (!isFormValid) {
     event.preventDefault();
-    error.innerText = 'Please Your Email Must be in Lower Case';
+
+    errors.classList.add('contact__form-errors--visible');
+    errors.innerText = 'Email must be in lowercase';
   }
 });
