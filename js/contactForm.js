d9257e5f73e3abe6259aab762e3dcf6e6b8ec91f
BHDHakim <hakim.bhd@gmail.com>
2022-05-11 18:02:54 +0100

    Refactore contactForm.js

--- js/contactForm.js
+++ js/contactForm.js
@@ -3,15 +3,10 @@ const email = document.getElementById('email');
 const error = document.getElementById('errors');
 
 form.addEventListener('submit', (event) => {
-    let a = false;
+  const isFormValid = email.value === email.value.toLowerCase();
+
+  if (!isFormValid) {
     event.preventDefault();
-    console.log(email.value,email.value.toLowerCase());
-    if(email.value === email.value.toLowerCase()) {
-       a = true;
-    }
-    if(a===false)
-    {
     error.innerText = 'Please Your Email Must be in Lower Case';
-    }
-  } );
-  
+  }
+});
